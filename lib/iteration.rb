44017e2bd54016fb650c1e4dbd7d30a77aeff85d
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-17 00:07:32 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -31,7 +31,7 @@ def find_greater_pair(src)
               element_index +=1 
             row_index +=1 
         end 
-           
+         end 
   
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
